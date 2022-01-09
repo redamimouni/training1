9a983ff888597b0c6368903372c45e1eea4370dd
Reda Mimouni <redamimouni@redas-macbook-air.home>
2022-01-09 20:29:48 +0100

    Init

---
+++ start/trainingApp/trainingApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  trainingApp
+//
+//  Created by Reda Mimouni on 09/01/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
