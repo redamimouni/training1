e8f9549062519a4073a8f5896c16b7afc824a313
Reda Mimouni <redamimouni@redas-macbook-air.home>
2022-02-27 23:21:38 +0100

    Init test

---
+++ test/TestSkills/TestSkills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSkills
+//
+//  Created by Reda Mimouni on 27/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
